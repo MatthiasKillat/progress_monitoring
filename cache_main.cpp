932a3264e45c2a40653277fd75fe8c8436139bf3
Matthias Killat <matthias.killat{at}gmx.de>
2022-10-05 17:26:08 +0200

    weak ref counting

--- cache_main.cpp
+++ cache_main.cpp
@@ -4,12 +4,49 @@
 
 int main(void) {
 
-   using MyCache = weak_cache<int, 4>;
+   using MyCache = weak_cache<int, 1>;
 
    MyCache cache;
 
   auto weak = cache.get_weak_ref();
+  weak.print();
+
+  {
+    auto strong = weak.get();
+    strong.print();
+
+    weak_ref<int> weak2(weak);
+    weak.print();
+    {
+      weak_ref<int> weak3(weak);
+      weak.print();
+    }
+    weak.print();
+  }
+  weak.print();
+
+  auto new_weak = cache.get_weak_ref();
+  new_weak.print();
+
   auto strong = weak.get();
+
+  std::cout << "***********" << std::endl;
+  new_weak.print(); // only one user (invalidated 1)
+  strong.print(); // nullref
+  new_weak.print();
+
+  auto strong1 = new_weak.get();
+  strong1.print();
+
+  new_weak.invalidate();
+  strong1.print();
+
+  //strong1.invalidate();
+  strong1.print();
+
+  auto new_weak2 = cache.get_weak_ref();
+  new_weak2.print();
+
 
   return EXIT_SUCCESS;
 }
--- include/cache.hpp
+++ include/cache.hpp
@@ -5,23 +5,25 @@
 #include <deque>
 #include <array>
 
+#include <iostream>
+
 using ref_t = uint64_t;
 using count_t = uint64_t;
 using index_t = uint32_t;
 
 template<typename T>
 struct control_block{
-    T value; // TODO: placement new type
+    T value; // TODO: placement new type (aligned memory)
     std::atomic<ref_t> strong{1}; // 0 unclaimed, 1 claimed by cache, exclusive, >=2 can be locked
     std::atomic<ref_t> weak{0};
     std::atomic<count_t> aba{0};
     std::atomic<index_t> index{0};
+   
+    // strong = 0 : unused
+    // strong = 1 : cache exclusive ownership, no change of strong from any ref
+    // strong = 2 : unused if weak = 0
 
-    // rules
-    // weak = 0 -> strong = 0
-    // strong = 1 : cache exclusive ownership, cannot be changed via cache
-
-    bool try_lock() {
+    bool try_strong_ref() {
         // c > 1 -> c + 1 
         auto old = strong.load();
         while(old > 1) {
@@ -36,9 +38,15 @@ struct control_block{
         --strong; //assume locked before
     }
 
+    // TODO: prove correctness and optimize
     bool claim() {
         // c == 0 -> 1
         ref_t exp = 0;
+        if(strong.compare_exchange_strong(exp, 1)) {
+            return true;
+        }
+        // c == 2 -> 1
+        exp = 2; // only weak refs
         if(strong.compare_exchange_strong(exp, 1)) {
             return true;
         }
@@ -46,8 +54,8 @@ struct control_block{
     }
 
     bool reclaim() {
-        // c == 1 -> 0
-        ref_t exp = 0;
+        // c == 2 -> 0
+        ref_t exp = 2;
         if(strong.compare_exchange_strong(exp, 0)) {
             return true;
         }
@@ -61,22 +69,21 @@ struct control_block{
 template<class T, index_t Capacity>
 class cache {
     static constexpr index_t DEFAULT_INDEX = 0;
-
-    // todo rename (block)
-    using slot = control_block<T>;
+    
+    using block = control_block<T>;
 
 public:
 
 cache()
 {
-    for(index_t i=1; i<=Capacity; ++i) {
+    for(index_t i=0; i<Capacity; ++i) {
         auto& s = m_slots[i];
         s.index = i;
         m_unused.push_back(i);
     }
 }
 
-slot& get_or(slot& alt) {
+block* get() {
     std::lock_guard<std::mutex> guard(m_mut); // TODO: no mutex later
     index_t i;
     
@@ -87,8 +94,8 @@ slot& get_or(slot& alt) {
 
             auto& s = get(i);
             claim_uncontended(s);
-            m_maybeUsed.push_back(i); // could have multiples now
-            return s;
+            m_maybeUsed.push_back(i);
+            return &s;
         }
 
         auto n = m_maybeUsed.size();
@@ -96,12 +103,13 @@ slot& get_or(slot& alt) {
         for(uint32_t i = 0; i<n; ++i) {
             if(!m_maybeUsed.empty()) {
                 i = m_maybeUsed.front();
-                m_maybeUsed.pop_front();
+                m_maybeUsed.pop_front();               
 
                 auto& s = get(i);
                 if(claim_contended(s)) { // may fail if used
                     m_maybeUsed.push_back(i);
-                    return s;
+                    // std::cout << "***1" << std::endl;
+                    return &s;
                 } else {
                     // it is now at the end of queue again
                     m_maybeUsed.push_back(i);
@@ -112,35 +120,35 @@ slot& get_or(slot& alt) {
         break;
     } while (true);
   
-    return alt;
+    return nullptr;
 }
 
-void ret(slot& slot) {
+void ret(block& block) {
 
-     if(slot.weak == 0) {
-        if(slot.reclaim()) {
+     if(block.weak == 0) {
+        if(block.reclaim()) {
             // there can be no strong refs
-            if(slot.weak == 0) {
+            if(block.weak == 0) { // required check?
                 std::lock_guard<std::mutex> guard(m_mut);
-                m_unused.push_back(slot.index);
+                m_unused.push_back(block.index);
             } else {
                // still in maybeUsed
             }
         }
-     }     
+     }
 
 }
 
 private:
 
 std::mutex m_mut;
-// 0 is a special slot
-std::array<slot, Capacity + 1> m_slots;
+// 0 is a special block
+std::array<block, Capacity + 1> m_slots;
 std::deque<index_t> m_maybeUsed;
 std::deque<index_t> m_unused; // not needed to be a deque later
-std::atomic<count_t> m_count{0};
+std::atomic<count_t> m_count{73};
 
-slot& get(index_t i) {
+block& get(index_t i) {
     return m_slots[i];
 }
 
@@ -148,47 +156,28 @@ count_t update_count() {
     return m_count.fetch_add(1);
 }
 
-void claim_uncontended(slot& s) {
+void claim_uncontended(block& s) {
     //can assume weak == 0 == strong
-    s.weak.store(1);
     s.strong.store(1);
     s.aba = update_count();
     ++s.strong; // could set to 2 directly?
 }
 
-bool claim_contended(slot& s) {
+bool claim_contended(block& s) {
     if(!s.claim()) {
-        return false; // there was a strong ref
+        return false;
     }
     // now we know strong = 1, weak = ?
     // no strong refs can be created by others (requires strong >= 2)
 
     s.aba = update_count(); // will invalidate old weak refs
-    s.weak = 1; // we declare we are the only weak ref now
-
     ++s.strong; // now others may try to generate strong refs again but will fail due to aba
     return true;
 }
 
 };
 
-// from unused queue: we know there are no weak refs, can claim uncontended
-//
-// from maybe used queue
-// if strong > 0 -> skip
-// if no strong ref (strong = 0)
-//    if no weak ones -> unused, free to use
-// so weak > 0, strong = 0
-//    CAS strong 0 -> 1
-//    if fail, skip
-//    if succeed we have now at least 1 strong ref to it
-//    we know there were weak refs in the past
-//    change aba counter
-//    if strong is still 1, no one has locked it (and we can set weak refs to 1?)
-//    otherwise skip it
-
-
-// if the slot is used, cache holds one ref
+// if the block is used, cache holds one ref
 // cache gives a weak ref 
 // weak refs can generate strong refs
 // weak refs can generate weak refs
@@ -204,13 +193,67 @@ class weak_ref;
 template<typename T>
 class strong_ref {
     friend class weak_ref<T>;
-    // TODO: ref counting
+    using block_t = control_block<T>;
+
 public:
+    ~strong_ref() {
+        if(valid()) {
+            unref();
+        }
+    }
+
+    T* get() {
+        return &m_block->value;
+    }
+
+    T& operator*() {
+        return m_block->value;
+    }
+
+    bool valid() {
+        return m_block != nullptr;
+    }
+
+    void invalidate() {
+        if(valid()) {
+            unref();
+            m_block = nullptr;
+        }
+    }
+
+    void print() {
+        if(!valid()) {
+            std::cout << "strong nullref" << std::endl;
+            return;
+        }
+
+        std::cout << "strong ref i " << m_block->index << " s = " << m_block->strong << " w = " << m_block->weak 
+        << " aba = " << m_block->aba << std::endl;
+    }
 
 private:
-    control_block<T>* m_block;
+    block_t* m_block{nullptr};
 
-    strong_ref(control_block<T>& block) : m_block(&block) {
+    strong_ref(block_t& block) : m_block(&block) {
+    }
+
+    strong_ref() = default;
+
+    // note: copy is forbidden for simplicity, only create via weak_ref
+    // TODO: relax?
+    strong_ref(const strong_ref & other) = delete; // TODO: RVO problems
+
+    void unref() {
+        --m_block->strong;
+        // TODO: free it, need deleter in block (or static, if there is only one cache)
+        if(m_block->weak == 0) {
+            // no weak refs and we were the only strong ref
+            if(m_block->strong == 2) {
+                --m_block->strong;
+            }
+            --m_block->strong; // cache reference
+            // free(m_block);
+        }
     }
 };
 
@@ -220,27 +263,86 @@ class weak_ref {
     friend class weak_cache;
 
 public: 
-    // TODO: ref counting
+    ~weak_ref() {
+        if(valid()) {
+            unref();
+        }
+    }
+
+    weak_ref(const weak_ref &other) : m_block(other.m_block), m_aba(other.m_aba) {
+        if(valid()) {
+            ref();
+        }
+    }
 
     strong_ref<T> get() {
-        return strong_ref<T>(*m_block);
+        if(!valid()) {
+            return strong_ref<T>();
+        }
+
+        // valid
+        if(m_block->try_strong_ref()) {
+            if(aba_check()) {
+                return strong_ref<T>(*m_block);
+            }
+            m_block->unlock();
+        }
+
+        invalidate_unchecked();
+        return strong_ref<T>();
+    }
+
+    void print() {
+        if(!valid()) {
+            std::cout << "weak nullref" << std::endl;
+            return;
+        }
+        std::cout << "weak ref i " << m_block->index << " s = " << m_block->strong << " w = " << m_block->weak
+        << " aba = " << m_block->aba << "(" << m_aba << ")" << std::endl;
+    }
+
+    bool valid() {
+        return m_block != nullptr;
+    }
+
+    void invalidate() {
+        if(valid()) {
+            invalidate_unchecked();
+        }
     }
 
 private:
-    control_block<T>* m_block;
+    control_block<T>* m_block{nullptr};
     count_t m_aba;
 
     weak_ref(control_block<T>& block) : m_block(&block) {
+        ref();
         m_aba = m_block->aba;
     }
 
-    bool validate() {
-        return m_aba = m_block->aba.load();
+    weak_ref() = default;
 
+    bool aba_check() {
+        // better/more powerful sanity check needed/easily possible?
+        return m_aba == m_block->aba.load();
+    }
+
+    void ref() {
+        ++m_block->weak;
+    }
+
+    void unref() {
+        // TODO: free it, need deleter in block (or static, if there is only one cache)
+        if(--m_block->weak == 0) {
+            // free(m_block);
+        }
+    }
+
+    void invalidate_unchecked() {
+        unref();
+        m_block = nullptr;
     }
 };
-
-
 
 template<class T, index_t Capacity = 4>
 class weak_cache {
@@ -250,8 +352,11 @@ class weak_cache {
 public:
 
     weak_ref<T> get_weak_ref() {
-        auto& block = m_cache.get_or(m_defaultBlock); // todo: change API to pointer
-        return weak_ref<T>(block);
+        auto block = m_cache.get();
+        if(block) {
+             return weak_ref<T>(*block);
+        }
+        return weak_ref<T>();
     }
 
     // strong_ref<T> get_strong_ref() {
@@ -261,6 +366,5 @@ public:
     // returning is not required, will happen automatically
 
 private:
-    block m_defaultBlock;
     cache<T, Capacity> m_cache;
 };
